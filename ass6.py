4a601890da72b4fbf86dd3d4fc926ac725185d93
smridhisharma <42332898+smridhisharma@users.noreply.github.com>
2018-08-25 18:51:01 +0530

    Add files via upload

---
+++ ass6.py
@@ -0,0 +1,37 @@
+#FUNCTIONS
+#question-1
+def calculate(rad):
+    return(4*3.14*rad*rad)
+rad=int(input("enter radius"))
+print(calculate(rad))
+
+
+#question-2
+def perfect(num):
+    sum=0
+    for j in range(1,num):
+        if num%j==0:
+            sum=sum+j
+    if sum==num:
+        print(num)
+for i in range(1,1000):
+    perfect(i)
+
+
+#question-3
+def table(num):
+    for i in range(1,11):
+        print(num,"*",i,"=",num*i)
+num=int(input("enter a number"))
+table(num)
+
+#question-4
+def cal(num,por):
+    if por!= 0:
+        return (num*cal(num, por-1))
+    else:
+        return 1
+num=int(input("enter a number"))
+por=int(input("enter the power"))
+print(cal(num,por))
+
